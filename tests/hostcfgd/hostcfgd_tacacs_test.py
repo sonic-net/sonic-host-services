c319323d98d899cac086864af48babc4cfa93b7f
liuh-80 <liuh@microsoft.com>
2022-08-17 02:35:39 +0000

    Improve code and add test case

--- tests/hostcfgd/hostcfgd_tacacs_test.py
+++ tests/hostcfgd/hostcfgd_tacacs_test.py
@@ -114,3 +114,27 @@ class TestHostcfgdTACACS(TestCase):
         self.check_config(test_name, test_data, "config_db_local_and_tacacs")
         # test disable accounting
         self.check_config(test_name, test_data, "config_db_disable_accounting")
+
+    def test_hostcfgd_tacacs_enable_accounting(self):
+        """
+            Test TACACS hostcfd daemon enable accounting when enable authentication
+
+            Returns:
+                None
+        """
+        host_config_daemon = hostcfgd.HostConfigDaemon()
+
+        # mock run_cmd
+        command = ""
+        def mock_run_cmd(cmd, log_err=True, raise_exception=False):
+            command = cmd
+
+        host_config_daemon.run_cmd = mock_run_cmd
+
+        # check enable authentication will also enable accounting
+        config_data = {
+            'login': 'tacacs+'
+        }
+        host_config_daemon.aaacfg.aaa_update("authentication",config_data)
+
+        self.assertEqual(command, "config aaa accounting 'tacacs+ local'")
