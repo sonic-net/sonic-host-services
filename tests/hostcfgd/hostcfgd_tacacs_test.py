eae2b7d3bedfd087b2c9fd352a7b6dfcfe51a01a
liuh-80 <liuh@microsoft.com>
2023-01-13 16:04:43 +0000

    Add UT and fix code

--- tests/hostcfgd/hostcfgd_tacacs_test.py
+++ tests/hostcfgd/hostcfgd_tacacs_test.py
@@ -8,6 +8,7 @@ import subprocess
 from swsscommon import swsscommon
 
 from parameterized import parameterized
+from sonic_py_common.general import load_module_from_source
 from unittest import TestCase, mock
 from tests.hostcfgd.test_tacacs_vectors import HOSTCFGD_TEST_TACACS_VECTOR
 from tests.common.mock_configdb import MockConfigDb, MockDBConnector
@@ -23,11 +24,7 @@ sys.path.insert(0, modules_path)
 
 # Load the file under test
 hostcfgd_path = os.path.join(scripts_path, 'hostcfgd')
-loader = importlib.machinery.SourceFileLoader('hostcfgd', hostcfgd_path)
-spec = importlib.util.spec_from_loader(loader.name, loader)
-hostcfgd = importlib.util.module_from_spec(spec)
-loader.exec_module(hostcfgd)
-sys.modules['hostcfgd'] = hostcfgd
+hostcfgd = load_module_from_source('hostcfgd', hostcfgd_path)
 
 # Mock swsscommon classes
 hostcfgd.ConfigDBConnector = MockConfigDb
@@ -42,13 +39,10 @@ class TestHostcfgdTACACS(TestCase):
         return subprocess.check_output('diff -uR {} {} || true'.format(file1, file2), shell=True)
 
     """
-        Check different config
+        Mock hostcfgd
     """
-    def check_config(self, test_name, test_data, config_name):
+    def mock_hostcfgd(self, test_data, config_name, op_path, sop_path):
         t_path = templates_path
-        op_path = output_path + "/" + test_name + "_" + config_name
-        sop_path = sample_output_path + "/" +  test_name + "_" + config_name
-
         hostcfgd.PAM_AUTH_CONF_TEMPLATE = t_path + "/common-auth-sonic.j2"
         hostcfgd.NSS_TACPLUS_CONF_TEMPLATE = t_path + "/tacplus_nss.conf.j2"
         hostcfgd.NSS_RADIUS_CONF_TEMPLATE = t_path + "/radius_nss.conf.j2"
@@ -68,8 +62,12 @@ class TestHostcfgdTACACS(TestCase):
         shutil.copyfile( sop_path + "/login.old", op_path + "/login")
 
         MockConfigDb.set_config_db(test_data[config_name])
-        host_config_daemon = hostcfgd.HostConfigDaemon()
+        return hostcfgd.HostConfigDaemon()
 
+    """
+        Render different config
+    """
+    def render_config_file(self, host_config_daemon):
         aaa = host_config_daemon.config_db.get_table('AAA')
 
         try:
@@ -83,6 +81,17 @@ class TestHostcfgdTACACS(TestCase):
             tacacs_server = []
 
         host_config_daemon.aaacfg.load(aaa,tacacs_global,tacacs_server,[],[])
+
+    """
+        Check different config
+    """
+    def check_config(self, test_name, test_data, config_name):
+        op_path = output_path + "/" + test_name + "_" + config_name
+        sop_path = sample_output_path + "/" +  test_name + "_" + config_name
+        host_config_daemon = self.mock_hostcfgd(test_data, config_name, op_path, sop_path)
+
+        self.render_config_file(host_config_daemon)
+
         dcmp = filecmp.dircmp(sop_path, op_path)
         diff_output = ""
         for name in dcmp.diff_files:
@@ -114,3 +123,53 @@ class TestHostcfgdTACACS(TestCase):
         self.check_config(test_name, test_data, "config_db_local_and_tacacs")
         # test disable accounting
         self.check_config(test_name, test_data, "config_db_disable_accounting")
+
+    @parameterized.expand(HOSTCFGD_TEST_TACACS_VECTOR)
+    def test_hostcfgd_sshd_integrity(self, test_name, test_data):
+        """
+            Test hostcfd sshd config file integrity check
+
+            Args:
+                test_name(str): test name
+                test_data(dict): test data which contains initial Config Db tables, and expected results
+
+            Returns:
+                None
+        """
+        config_name = "config_db_local"
+        op_path = output_path + "/" + test_name + "_" + config_name
+        sop_path = sample_output_path + "/" +  test_name + "_" + config_name
+        host_config_daemon = self.mock_hostcfgd(test_data, config_name, op_path, sop_path)
+
+        # test sshd empty case
+        hostcfgd.ETC_PAMD_SSHD = op_path + "/sshd_empty"
+        shutil.copyfile( sop_path + "/sshd_empty.old", op_path + "/sshd_empty")
+
+        # render with empty sshd config file and check error log
+        original_syslog = hostcfgd.syslog
+        with mock.patch('hostcfgd.syslog.syslog') as mocked_syslog:
+            mocked_syslog.LOG_ERR = original_syslog.LOG_ERR
+            self.render_config_file(host_config_daemon)
+
+            # check sys log
+            expected = [
+                mock.call(mocked_syslog.LOG_ERR, "file integrity check failed: {} is empty, file corrupted".format(hostcfgd.ETC_PAMD_SSHD))
+            ]
+            mocked_syslog.assert_has_calls(expected)
+
+        # test sshd missing case
+        hostcfgd.ETC_PAMD_SSHD = op_path + "/sshd_missing"
+
+        with mock.patch('hostcfgd.syslog.syslog') as mocked_syslog:
+            mocked_syslog.LOG_ERR = original_syslog.LOG_ERR
+
+            # missing file can't test by render config file,
+            # because missing file case difficult to reproduce: code always generate a empty file.
+            host_config_daemon.aaacfg.check_file_integrity(hostcfgd.ETC_PAMD_SSHD)
+
+            # check sys log
+            expected = [
+                mock.call(mocked_syslog.LOG_ERR, "file integrity check failed: {} is missing".format(hostcfgd.ETC_PAMD_SSHD))
+            ]
+            mocked_syslog.assert_has_calls(expected)
+
