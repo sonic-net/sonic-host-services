6b8e7ad8eb1a6047efade01db2f89b2d617b57e9
maipbui <maibui@microsoft.com>
2023-01-13 16:19:37 +0000

    Revert "Revert "Remove subprocess with shell=True (#24)" (#33)"
    This reverts commit 28afb558dc5405ece2afb84f903c4085e6e5bbb2.

--- tests/hostcfgd/hostcfgd_passwh_test.py
+++ tests/hostcfgd/hostcfgd_passwh_test.py
@@ -9,6 +9,7 @@ import re
 
 from parameterized import parameterized
 from unittest import TestCase, mock
+from sonic_py_common.general import getstatusoutput_noshell
 from tests.hostcfgd.test_passwh_vectors import HOSTCFGD_TEST_PASSWH_VECTOR
 from tests.common.mock_configdb import MockConfigDb, MockDBConnector
 
@@ -44,7 +45,7 @@ class TestHostcfgdPASSWH(TestCase):
     """
     def run_diff(self, file1, file2):
         try:
-            diff_out = subprocess.check_output('diff -ur {} {} || true'.format(file1, file2), shell=True)
+            _, diff_out = getstatusoutput_noshell(['diff', '-ur', file1, file2])
             return diff_out
         except subprocess.CalledProcessError as err:
             syslog.syslog(syslog.LOG_ERR, "{} - failed: return code - {}, output:\n{}".format(err.cmd, err.returncode, err.output))
