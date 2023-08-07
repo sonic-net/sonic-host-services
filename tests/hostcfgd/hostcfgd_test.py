b035aad49d0d1cae854181d1419ca46b98112fb9
Ivan Davydenko <ivanda@nvidia.com>
2023-08-07 18:08:04 +0300

    [cli-sessions]  update hostcfgd: add serial-console and extend ssh-server modules,  update existing tests,  introduce serial-console test

--- tests/hostcfgd/hostcfgd_test.py
+++ tests/hostcfgd/hostcfgd_test.py
@@ -121,6 +121,48 @@ class TesNtpCfgd(TestCase):
             ])
 
 
+class TestSerialConsoleCfgd(TestCase):
+    """
+        Test hostcfd daemon - SerialConsoleCfg
+    """
+    def setUp(self):
+        MockConfigDb.CONFIG_DB['SERIAL_CONSOLE'] = {
+            'POLICIES': {'inactivity-timeout': '15', 'sysrq-capabilities': 'disabled'}
+        }
+
+    def tearDown(self):
+        MockConfigDb.CONFIG_DB = {}
+
+    def test_serial_console_update_cfg(self):
+        with mock.patch('hostcfgd.subprocess') as mocked_subprocess:
+            popen_mock = mock.Mock()
+            attrs = {'communicate.return_value': ('output', 'error')}
+            popen_mock.configure_mock(**attrs)
+            mocked_subprocess.Popen.return_value = popen_mock
+
+            serialcfg = hostcfgd.SerialConsoleCfg()
+            serialcfg.update_serial_console_cfg(
+                'POLICIES', MockConfigDb.CONFIG_DB['SERIAL_CONSOLE']['POLICIES'])
+            mocked_subprocess.check_call.assert_has_calls([
+                call('sudo service serial-config restart'),
+            ])
+
+    def test_serial_console_is_caching_config(self):
+        with mock.patch('hostcfgd.subprocess') as mocked_subprocess:
+            popen_mock = mock.Mock()
+            attrs = {'communicate.return_value': ('output', 'error')}
+            popen_mock.configure_mock(**attrs)
+            mocked_subprocess.Popen.return_value = popen_mock
+
+            serialcfg = hostcfgd.SerialConsoleCfg()
+            serialcfg.cache['POLICIES'] = MockConfigDb.CONFIG_DB['SERIAL_CONSOLE']['POLICIES']
+
+            serialcfg.update_serial_console_cfg(
+                'POLICIES', MockConfigDb.CONFIG_DB['SERIAL_CONSOLE']['POLICIES'])
+
+            mocked_subprocess.check_call.assert_not_called()
+
+
 class TestHostcfgdDaemon(TestCase):
 
     def setUp(self):
