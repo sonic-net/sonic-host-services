db641630210fd4fb3973557f4ab418a1a0501882
Julian Chang - TW <julianc@supermicro.com.tw>
2025-04-17 16:17:24 +0000

    Convert skip_mesg_auth from literal to boolean.
    Signed-off-by: Julian Chang - TW <julianc@supermicro.com.tw>

--- tests/hostcfgd/test_radius_vectors.py
+++ tests/hostcfgd/test_radius_vectors.py
@@ -90,7 +90,7 @@ HOSTCFGD_TEST_RADIUS_VECTOR = [
                         "retransmit": "1",
                         "timeout": "1",
                         "passkey": "pass1",
-                        "skip_msg_auth": "false",
+                        "skip_msg_auth": "true",
                     }
                 },
             },
@@ -159,7 +159,7 @@ HOSTCFGD_TEST_RADIUS_VECTOR = [
                         "retransmit": "1",
                         "timeout": "1",
                         "passkey": "pass1",
-                        "skip_msg_auth": "false",
+                        "skip_msg_auth": "true",
                     }
                 },
             },
