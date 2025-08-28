9d4476c530f4621a92ec35e1afc1de03c6386419
Liu Shilong <shilongliu@microsoft.com>
2025-08-28 11:07:41 +0800

    Pin PyGObject to 3.42.2

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'dbus-python',
         'systemd-python',
         'Jinja2>=2.10',
-        'PyGObject==3.50.0',
+        'PyGObject==3.42.2',
         'pycairo==1.26.1',
         'psutil'
     ] + sonic_dependencies,
