9551a7feda702316c237708195aa0dbb42e94ae6
Ramesh Raghupathy <ram@cisco.com>
2025-11-19 15:10:45 -0800

    added specific exception type

--- scripts/check_platform.py
+++ scripts/check_platform.py
@@ -15,7 +15,7 @@ def main():
             sys.exit(0)
         else:
             sys.exit(1)
-    except Exception:
+    except (ImportError, AttributeError, RuntimeError):
         sys.exit(1)
 
 if __name__ == "__main__":
